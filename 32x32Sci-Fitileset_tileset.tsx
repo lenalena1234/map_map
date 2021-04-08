3a296c5755badbdb6cc2582d5cfb0cd5675ff7c4
lenalena1234 <82158764+lenalena1234@users.noreply.github.com>
2021-04-08 13:48:11 +0000

    Deploying to gh-pages from master @ bc023969f609f47444428f9630beea7d6a6f0179 🚀

---
+++ 32x32Sci-Fitileset_tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.5.0" name="32x32Sci-Fitileset_tileset" tilewidth="32" tileheight="32" tilecount="512" columns="32">
+ <image source="workadventure/tilesets/32x32Sci-Fitileset_tileset.png" width="1024" height="512"/>
+</tileset>
